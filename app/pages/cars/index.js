fd0d46109c07da9fead9070e79ff586119555f24
jaden <hanpanpan@outlook.com>
2019-10-28 22:10:00 +0800

    增加购物车页面

---
+++ app/pages/cars/index.js
@@ -0,0 +1,123 @@
+// pages/cars/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cars:[
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      {
+        id: 1,
+        title: "异能穿越：狂吼芸兰（）超赞双玻",
+        descript: "免费!声音超级甜的小姑娘主播，异能加持，各种吊打，非常好看",
+        studyNum: 20,
+        img: '//imagev2.xmcdn.com/group59/M00/94/36/wKgLeFzoOxaBX1wzAAGulZyb_K8517.jpg'
+      },
+      
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  onClickLeft() {
+    wx.showToast({ title: '点击返回', icon: 'none' });
+  },
+  onClickRight() {
+    wx.showToast({ title: '点击按钮', icon: 'none' });
+  }
+})
